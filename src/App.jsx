ce1a249407e1438d256908afab58ac8315019867
zenteno125 <zenteno125gamer@gmail.com>
2023-11-13 03:31:36 -0600

    Login-Register

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Login from "./components/login"
+import Routers from "./routes/Routers"
+
+function App() {
+  
+
+  return (
+    <>
+        <Routers />    
+    </>
+  )
+}
+
+export default App
